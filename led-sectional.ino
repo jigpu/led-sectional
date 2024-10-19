00f77a38db79c4c57d452a3d6c47d2c892d86a16
Jason Gerecke <killertofu@gmail.com>
2024-10-19 12:36:17 -0700

    Start using new aviationweather.gov API
    Aviationweather.gov introduced a new API endpoint which may be replacing
    cgi-bin at some point in the future. Start making calls to this API
    endpoint instead.
    
    Link: https://aviationweather.gov/data/api/help/
    Link: https://aviationweather.gov/help/changelog/

--- led-sectional.ino
+++ led-sectional.ino
@@ -147,7 +147,7 @@ std::vector<unsigned short int> lightningLeds;
 #define RETRY_TIMEOUT 15000 // in ms
 
 #define SERVER "aviationweather.gov"
-#define BASE_URI "/cgi-bin/data/dataserver.php?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecentForEachStation=true&stationString="
+#define BASE_URI "/api/data/metar?format=xml&ids="
 
 boolean ledStatus = true; // used so leds only indicate connection status on first boot, or after failure
 int loops = -1;
